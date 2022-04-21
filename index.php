ad2e65c5a925d46b87b96a91cc3b3d178df9ec50
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-04-21 17:01:40 +0200

    added ordered lists  main

--- index.php
+++ index.php
@@ -6,9 +6,51 @@
 
 
         "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?" => " <p> Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno. </p> <p> Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo. </p> <p> Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href=\"https://www.google.com/safetycenter/\">Centro Google per la sicurezza online</a>.</p> <p> Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo. </p>",
+        //? tentativo andato malissimo
 
-        /* perché il mio account é associato a un paese? */
+/*         $array = array(
+            "Perché il mio account è associato a un paese?" => array(
+                "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose" => array(
+                    "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:" => array(
+                        "a" => "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera",
+                        "b" => "Google LLC, con sede negli Stati Uniti, per il resto del mondo.",
+                    ),
+                    "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
+                ),
+                "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
+                "<h5>Stabilire il paese associato al tuo account</h5>" => "<p>Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.</p> <p>I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.</p><p>Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.</p>",
+            )
+        ), */
 
+        "Perché il mio account è associato a un paese?" => "
+        <ul>
+            <li>
+                <p>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</p>
+                    <ol>
+                        <li>
+                        <p> La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</p>
+                            <ol>
+                                <li>
+                                <p>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</p>
+                                </li>
+                                <li>
+                                <p>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</p>
+                                </li>
+                            </ol>
+                        </li>
+                        <li>
+                            <p>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</p>
+                        </li>
+                    </ol>
+            </li>
+            <li>
+                <p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>
+            </li>
+        </ul>
+        
+        <h4>Stabilire il paese associato al tuo account</h4> <p>Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.</p> <p>I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.</p><p>Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.</p>
+        ",
+        
         "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?" => " <p> I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web,quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href=\"https://support.google.com/websearch/answer/9109?hl=it\">contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href=\"https://support.google.com/legal/contact/lr_eudpa?product=websearch&hl=it\">fao clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href=\"https://support.google.com/websearch/troubleshooter/3111061?hl=it\"> visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>.</p>",
 
 
@@ -31,20 +73,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- style css -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Google FAQ</title>
 </head>
 <body>
     <header></header>
     <main class="container">
-
-    <?php  foreach ($faqs as $question => $answer){ ?>
-        <h1><?php echo $question?></h1>
-        <!-- <p><?php var_dump(explode('.', $answer))  ?></p> -->
-        <p><?php echo $answer ?></p>
-    <?php }; ?>
-
-    
+        <section class="row">
+            <div class="col-12">
+                <?php  foreach ($faqs as $question => $answer){ ?>
+                    <h1><?php echo $question?></h1>
+                    <!-- <p><?php var_dump(explode('.', $answer))  ?></p> -->
+                    <p><?php echo $answer ?></p>
+                <?php }; ?>
+            </div>
+        </section>
     </main>
     <footer></footer>
 </body>
